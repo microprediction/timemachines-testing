d6f0a1a29fd6088453db01b631d2b6d1aa63c1d0
Peter Cotton <peter.cotton@microprediction.com>
2021-01-20 10:39:49 -0500

    clean out platypus errors

--- matchups.py
+++ matchups.py
@@ -46,44 +46,30 @@ def random_optimizer_matchup():
     evaluator = random.choice(EVALUATORS)
     names = mw.get_stream_names()
     n_lagged = 0
-    n = random.choice([150])
+    n = 2000
     while n_lagged<n:
         name = random.choice(names)
         ys = list(reversed(mw.get_lagged_values(name=name, count=2000)))
         n_lagged = len(ys)
 
-    n_trials = random.choice([5,10,20])
+    n_trials = random.choice([20,40,80])
     n_dim = random.choice([2,3])
-    n_burn = n-50
+    n_burn = min(n-50,100)
 
     # White plays
+    white_valid = False
     print('White is '+white.__name__)
-    start_time = time.time()
-    best_val_white, best_x_white, count_white = optimize(f=f, ys=ys, n_trials=n_trials, n_dim=n_dim, n_burn=n_burn, optimizer=white, evaluator=evaluator, with_count=True)
-    white_elapsed = time.time()-start_time
-
-    # Black plays
-    print('Black is ' + black.__name__)
-    start_time = time.time()
-    best_val_black, best_x_black, count_black = optimize(f=f, ys=ys, n_trials=n_trials, n_dim=n_dim, n_burn=n_burn, optimizer=black, evaluator=evaluator, with_count=True)
-    black_elapsed = time.time()-start_time
-
-    valid_white, valid_black = count_white < 2 * n_trials, count_black <= n_trials
-
-    tol = 1e-3 * (abs(best_val_white) + abs(best_val_black))
-    if valid_white and valid_black:
-        if best_val_white < best_val_black - tol:
-            points = 1.0
-        elif best_val_black < best_val_white - tol:
-            points = 0.0
-        else:
-            points = 0.5
-    else:
-        points = None
+    white_n = n_trials
+    while white_valid is False:
+        print('Starting White optimization')
+        start_time = time.time()
+        best_val_white, best_x_white, count_white = optimize(f=f, ys=ys, n_trials=white_n, n_dim=n_dim, n_burn=n_burn, optimizer=white, evaluator=evaluator, with_count=True)
+        white_elapsed = time.time()-start_time
+        white_valid = count_white < 2*n_trials+10
+        white_n = int( n_trials / 2 )
 
     report = {'white': optimizer_name(white),
               'black': optimizer_name(black),
-              'points':points,
               'epoch_time':time.time(),
               'f': f.__name__,
               'name': name,
@@ -96,14 +82,31 @@ def random_optimizer_matchup():
               'evaluator': evaluator.__name__,
               'n': n, 'n_trials': n_trials, 'n_dim': n_dim, 'n_burn': n_burn,
               'white_elapsed':white_elapsed,
-              'black_elapsed':black_elapsed,
               'best_val_white':best_val_white,
-              'best_val_black':best_val_black,
               'count_white':count_white,
-              'count_black':count_black,
-              'valid_white':valid_white,
-              'valid_black':valid_black
               }
+    pprint(report)
+
+    # Black plays
+    print('Black is ' + black.__name__)
+    start_time = time.time()
+    best_val_black, best_x_black, count_black = optimize(f=f, ys=ys, n_trials=n_trials, n_dim=n_dim, n_burn=n_burn, optimizer=black, evaluator=evaluator, with_count=True)
+    black_elapsed = time.time()-start_time
+    valid_white = count_white <= 1.2*n_trials
+    valid_black = count_black <= 1.2*count_white
+
+    tol = 1e-3 * (abs(best_val_white) + abs(best_val_black))
+    if valid_white and valid_black:
+        if best_val_white < best_val_black - tol:
+            points = 1.0
+        elif best_val_black < best_val_white - tol:
+            points = 0.0
+        else:
+            points = 0.5
+    else:
+        points = None
+
+    report.update({'points':points,'valid_white':valid_white,'valid_black':valid_black,'white_elapsed':white_elapsed,'black_elapsed':black_elapsed})
 
     return report
 
